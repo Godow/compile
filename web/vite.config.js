a6e8277fc09f22b6ee8a7ad1a96b2bd92b86348c
godow <zhs3709@163.com>
2022-09-11 14:28:12 +0800

    fix

--- web/vite.config.js
+++ web/vite.config.js
@@ -7,6 +7,8 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
+import alias from "@rollup/plugin-alias";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,6 +19,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    alias(),
   ],
   resolve: {
     alias: {
